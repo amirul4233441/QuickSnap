9cd5ad66a81de659130adce027dacecfeb51fe31
ronimolla <mollaroni12@gmail.com>
2018-03-13 22:19:05 +0800

    first commit

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -17,15 +17,29 @@ namespace CardGames
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
 		/// <param name="myGame">The game object to update in response to events.</param>
-private static void HandleUserInput (Snap myGame)
-{
-	//Fetch the next batch of UI interaction
-	SwinGame.ProcessEvents ();
+		private static void HandleUserInput(Snap myGame)
+		{
+			//Fetch the next batch of UI interaction
+			SwinGame.ProcessEvents();
 
-	if (SwinGame.KeyTyped (KeyCode.vk_SPACE)) {
-		myGame.FlipNextCard ();
-	}
-}
+			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
+			{
+				myGame.Start ();
+			}
+
+			if (myGame.IsStarted) 
+			{
+				if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT) &&
+					SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
+					//TODO add sound effects
+				} else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT)) {
+					myGame.PlayerHit (0);
+				} else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
+					myGame.PlayerHit (1);
+				}
+			}
+		}
+
 		/// <summary>
 		/// Draws the game to the Window.
 		/// </summary>
@@ -49,7 +63,7 @@ private static void HandleUserInput (Snap myGame)
 			}
 
 			// Draw the back of the cards... to represent the deck
-			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 155, 153);
+			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 15, 50);
 
 			//Draw onto the screen
 			SwinGame.RefreshScreen(60);
